0c4e81eadc0da8f773c08dee82bfe809e256887f
pangdeng <2273937024@qq.com>
2022-04-26 15:28:31 +0800

    哈哈哈

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,18 @@
 //在这个函数中可以拿到我们给ajax提供的配置对象
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
     //发起真正的ajax请求前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
-    console.log(options.url);
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.
+            message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
